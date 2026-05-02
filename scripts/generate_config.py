2bca8f7983a03e437ebd2b34dc41523175c49a41
Sunil Burigen <69686572+burigensunil88@users.noreply.github.com>
2026-05-02 12:13:30 -0700

    Junos initial automation project

---
+++ scripts/generate_config.py
@@ -0,0 +1,28 @@
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+# Load YAML data
+with open('../inventory/device.yaml') as f:
+    data = yaml.safe_load(f)
+
+# Load template
+env = Environment(loader=FileSystemLoader('../templates'))
+template = env.get_template('interface.j2')
+
+# Generate configs
+for device in data['devices']:
+
+    config = template.render(
+        hostname=device['hostname'],
+        interface=device['interface'],
+        description=device['description'],
+        ip=device['ip'],
+        prefix=device['prefix']
+    )
+
+    filename = f"../configs/{device['hostname']}.conf"
+
+    with open(filename, 'w') as output:
+        output.write(config)
+
+    print(f"Generated config for {device['hostname']}")
